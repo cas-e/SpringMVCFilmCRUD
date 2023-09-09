a2408b01430fd6c79f82584a3819fecf004b3fa2
cas-e <caseyk6.28@gmail.com>
2023-09-09 08:33:19 -0700

    add some error handling for empty string in getFilm()

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -18,7 +18,7 @@ public class FilmController {
 	
 	/*
 	 * I could not get this method to work...
-	 * I don't understand the Model.addAtrribute ?
+	 * I don't understand the Model.addAtrribute()
 	 */
 //	@RequestMapping(path = "getFilm.do", method=RequestMethod.GET)
 //	public String getFilm(@RequestParam Integer filmId, Model model) {
@@ -29,18 +29,25 @@ public class FilmController {
 //
 	/*
 	 * This method is like the one we used in USstates, it seems to be working. 
-	 * 
-	 * However, this method does fail when a user passes in an empty string.
 	 */
 	@RequestMapping(path = "getFilm.do", params="filmId", method=RequestMethod.GET)
-	public ModelAndView getFilm(int filmId) {
+	public ModelAndView getFilm(String filmId) {
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("WEB-INF/film.jsp");
 		
-		Film film = filmDao.findFilmById(filmId);
 		
+		Film film = null;
 		String notFound = "Sorry, the film with the ID: " + filmId + " does not exist in our records.";
 		
+		try {
+			int id = Integer.parseInt(filmId);
+			film = filmDao.findFilmById(id);
+			
+		} catch(Exception e) {
+			// if we can't parse the filmId, film remains null.
+		}
+	
 		if (film != null) {
 			mv.addObject("filmId", film);
 		} else {
@@ -49,5 +56,8 @@ public class FilmController {
 		
 		return mv;
 	}
+	
+	
+	
 	
 }
