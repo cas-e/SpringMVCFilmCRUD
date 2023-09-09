a3a4a728681c528f2048d8a35af11259b643e10c
TJReid9 <tjreid20@icloud.com>
2023-09-09 11:05:03 -0600

    Updated add Film

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -11,14 +11,13 @@ import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
-	
-	
+
 	@Autowired
 	private FilmDAO filmDao;
-	
+
 	/*
-	 * I could not get this method to work...
-	 * I don't understand the Model.addAtrribute()
+	 * I could not get this method to work... I don't understand the
+	 * Model.addAtrribute()
 	 */
 //	@RequestMapping(path = "getFilm.do", method=RequestMethod.GET)
 //	public String getFilm(@RequestParam Integer filmId, Model model) {
@@ -28,36 +27,50 @@ public class FilmController {
 //		return "WEB-INF/film.jsp";
 //
 	/*
-	 * This method is like the one we used in USstates, it seems to be working. 
+	 * This method is like the one we used in USstates, it seems to be working.
 	 */
-	@RequestMapping(path = "getFilm.do", params="filmId", method=RequestMethod.GET)
+	@RequestMapping(path = "getFilm.do", params = "filmId", method = RequestMethod.GET)
 	public ModelAndView getFilm(String filmId) {
-		
+
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("WEB-INF/film.jsp");
-		
-		
+
 		Film film = null;
 		String notFound = "Sorry, the film with the ID: " + filmId + " does not exist in our records.";
-		
+
 		try {
 			int id = Integer.parseInt(filmId);
 			film = filmDao.findFilmById(id);
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			// if we can't parse the filmId, film remains null.
 		}
-	
+
 		if (film != null) {
 			mv.addObject("filmId", film);
 		} else {
 			mv.addObject("filmId", notFound);
 		}
-		
+
 		return mv;
 	}
-	
-	
-	
-	
+
+	@RequestMapping(path = "createFilm.do", params = "filmTitle", method = RequestMethod.GET)
+	public ModelAndView createFilm(String filmTitle) {
+
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("WEB-INF/film.jsp");
+
+		Film film = new Film();
+		String notFound = "Sorry, the film with the ID: " + filmTitle + " does not exist in our records.";
+
+		film.setTitle(filmTitle);
+		film.setLanguageId(1);
+		filmDao.createFilm(film);
+
+		mv.addObject("film", film);
+
+		return mv;
+	}
+
 }
